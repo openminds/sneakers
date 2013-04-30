d70fe6d460a3d294a9bee8332aa7955e68a6581f
Dimitri Steyaert <github@dimitri.eu>
2013-04-30 11:33:48 +0200

    [chef] adds phpmyadmin cookbook

---
+++ chef/cookbooks/phpmyadmin/metadata.rb
@@ -0,0 +1,8 @@
+name             'phpmyadmin'
+maintainer       'Openminds BVBA'
+maintainer_email 'support@openminds.be'
+license          'Apache 2.0'
+description      'Installs/Configures phpmyadmin'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.1'
+depends          'apt'
---
+++ chef/cookbooks/phpmyadmin/recipes/default.rb
@@ -0,0 +1,65 @@
+latest_version = "3.5.4"
+service "apache2"
+service "php5-fpm"
+
+
+if (%x[egrep -i "^phpmyadmin" /etc/group].empty? == true) && (%x[grep phpmyadmin /etc/group].empty? == false)
+  raise "phpmyadmin cookbook was refactored. Please do `deluser phpmyadmin` and rerun OR remove phpmyadmin cookbook from runlist. -Steven"
+end
+
+user "phpmyadmin" do
+  comment "phpmyadmin"
+  shell "/bin/false"
+end
+
+directory "/home/phpmyadmin/" do
+  owner "phpmyadmin"
+  group "phpmyadmin"
+  recursive true
+end
+
+cookbook_file "phpMyAdmin--all-languages.tar.bz2" do
+  path "/tmp/phpMyAdmin-all-languages.tar.bz2"
+  source "phpMyAdmin-#{latest_version}-all-languages.tar.bz2"
+  mode "0755"
+end
+
+directory "/home/phpmyadmin/default_www" do
+  owner "phpmyadmin"
+  group "phpmyadmin"
+  mode 00755
+  action :create
+end
+
+
+execute "extract phpmyadmin" do
+  command "tar xf /tmp/phpMyAdmin-all-languages.tar.bz2 -C /home/phpmyadmin/default_www; chown -Rf phpmyadmin:phpmyadmin /home/phpmyadmin/default_www"
+  not_if "test -f /home/phpmyadmin/default_www/config.inc.php"
+end
+
+directory "/home/phpmyadmin/default_www/setup" do
+  recursive true
+  action :delete #setup is dangerous, remove if found!
+end
+
+template "config.inc.php" do
+  path "/home/phpmyadmin/default_www/config.inc.php"
+  source "config.inc.php.erb"
+  variables(
+  :hash => Digest::SHA1.hexdigest((node[:hostname]+node[:ipaddress]).to_s)
+  )
+  owner "phpmyadmin"
+  mode "0644"
+end
+
+php_fpm_fgci_string = "FastCGIExternalServer /usr/sbin/php-fpm-phpmyadmin -socket /var/run/php_fpm_phpmyadmin.sock -idle-timeout 600 -pass-header Authorization"
+execute "Add the php-fpm socket to the fpm-sockets-file for phpmyadmin" do
+  command "echo '#{php_fpm_fgci_string}' >> /etc/apache2/conf.d/php-fpm-fcgi-servers"
+  # als de fpm fcgi string al geconfigureerd is: NIET doen.
+  # als de user niet bestaat: NIET doen.
+  # als de fcgi string nog niet geconfigureerd, en de user bestaat: WEL doen.
+  not_if "grep '#{php_fpm_fgci_string}' /etc/apache2/conf.d/php-fpm-fcgi-servers || (id phpmyadmin; if [ \"\$?\" -ne \"0\" ]; then exit 0; else exit 1; fi)"
+  action :run
+  notifies :restart, "service[apache2]"
+  notifies :restart, "service[php5-fpm]"
+end
