684d299286d49447ffaeeb5ced8b1980e5f36311
Steven De Coeyer <steven@banteng.be>
2013-05-02 14:18:50 +0200

    [chef nginx] sets 8080 instead of node[:base][:app_settings].admin_port.to_s

--- chef/cookbooks/nginx/recipes/proxy.rb
+++ chef/cookbooks/nginx/recipes/proxy.rb
@@ -20,7 +20,7 @@ template "/etc/nginx/conf.d/default.conf" do
   group "root"
   mode "0644"
   variables(
-    :backend_port => node[:base][:app_settings].admin_port.to_s,
+    :backend_port => 8080,
     :listen_port => node[:base][:app_settings].http_port.to_s
   )
   notifies :reload, "service[nginx]"
