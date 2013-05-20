5c7ff8e37d97ed8432af89deb156b71b75a84265
Bert Pattyn <bert@dextrose.be>
2013-05-20 18:40:44 +0200

    replace some values in php.ini template by attributes

--- chef/cookbooks/apache/attributes/php.rb
+++ chef/cookbooks/apache/attributes/php.rb
@@ -1,2 +1,10 @@
 default[:php][:version] = "php53"
 default[:php][:timezone] = "Europe/Brussels"
+default[:php][:html_errors] = "Off"
+default[:php][:max_execution_time] = 60
+default[:php][:max_input_time] = 60
+default[:php][:memory_limit] = "192M"
+default[:php][:error_reporting] = "E_ALL & ~E_DEPRECATED"
+default[:php][:display_errors] = "Off"
+default[:php][:display_startup_errors] = "Off"
+default[:php][:upload_max_filesize] = "32M"
