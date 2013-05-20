8b6f7620cb30ce91718a800549d07f176b0d94f9
Bert Pattyn <bert@dextrose.be>
2013-05-20 19:48:51 +0200

    add test for xdebug

---
+++ chef/cookbooks/apache/files/default/tests/minitest/php_xdebug_test.rb
@@ -0,0 +1,17 @@
+class TestPhp < MiniTest::Chef::TestCase
+
+  def test_that_xdebug_is_enabled
+      assert system("php -i | grep 'xdebug support => enabled'")
+  end
+
+  def test_that_the_xdebug_config_file_is_added
+    assert File.exists?("/etc/php5/fpm/conf.d/20-xdebug.ini")
+  end
+
+  def test_that_xdebug_is_installed
+    %w[php5-xdebug].each do |pkg|
+      assert system("dpkg -l | grep #{pkg}")
+    end
+  end
+
+end
