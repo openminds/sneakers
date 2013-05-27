2b9edb23c88132672912b95e6f6d2b5935008f77
Steven De Coeyer <steven@banteng.be>
2013-05-27 11:14:29 +0200

    [chef nginx] Fixes windows CR+LF to CR

--- chef/cookbooks/nginx/recipes/configuration.rb
+++ chef/cookbooks/nginx/recipes/configuration.rb
@@ -6,6 +6,7 @@ cookbook_file "/etc/init.d/nginx" do
   owner "root"
   action :create
   notifies :start, "service[nginx]"
+  notifies :run, "execute[Fix windows <CR>]"
 end
 
 cookbook_file "/etc/nginx/mime.types" do
@@ -17,4 +18,9 @@ directory "/var/log/nginx/"
 cookbook_file "/etc/nginx/nginx.conf" do
   source "nginx.conf"
   notifies :restart, "service[nginx]"
+end
+
+execute "Fix windows <CR>" do
+  command "perl -i -pe's/\r$//;' /etc/init.d/nginx"
+  action :run
 end
