8a9a2fcf90426eb165f9aad5f153837c4fe69320
Bert Pattyn <bert@dextrose.be>
2013-06-03 20:19:11 +0200

    add test for wkhtmltopdf

--- chef/cookbooks/extra/files/default/tests/minitest/wkhtmltopdf_test.rb
+++ chef/cookbooks/extra/files/default/tests/minitest/wkhtmltopdf_test.rb
@@ -1,11 +1,7 @@
 class TestWkhtmltopdf < MiniTest::Chef::TestCase
 
   def test_that_wkhtmltopdf_is_installed
-    assert system("dpkg -l | grep wkhtmltopdf")
-  end
-
-  def test_that_xvfb_is_installed
-    assert system("dpkg -l | grep xvfb")
+    assert File.exists?('/usr/local/bin/wkhtmltopdf')
   end
 
 end
