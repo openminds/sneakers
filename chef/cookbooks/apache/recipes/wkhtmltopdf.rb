30afb3d39e549d69e5de81ef1da212ed49049fc9
Bert Pattyn <bert@dextrose.be>
2013-06-03 20:18:09 +0200

    add chef recipe to install wkhtmltopdf (static)

--- chef/cookbooks/apache/recipes/wkhtmltopdf.rb
+++
@@ -1,2 +0,0 @@
-package 'wkhtmltopdf'
-package 'xvfb'
--- chef/cookbooks/extra/metadata.rb
+++ chef/cookbooks/extra/metadata.rb
@@ -1,7 +1,7 @@
 name             'extra'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       ''
+maintainer_email ''
 license          'All rights reserved'
-description      'Installs/Configures extra'
+description      'Installs/Configures extra packages'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
