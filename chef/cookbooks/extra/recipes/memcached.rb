b5ee0d46c9a2e4f370e08e957e162e934110e3b4
Bert Pattyn <bert.pattyn@wijs.be>
2013-07-17 21:49:35 +0200

    add a template for memcached.conf

--- chef/cookbooks/extra/recipes/memcached.rb
+++ chef/cookbooks/extra/recipes/memcached.rb
@@ -3,3 +3,16 @@
     action :install
   end
 end
+
+service "memcached" do
+  action :nothing
+  supports :status => true, :start => true, :stop => true, :restart => true
+end
+
+template "/etc/memcached.conf" do
+  source "memcached.conf.erb"
+  owner "root"
+  group "root"
+  mode 00644
+  notifies :restart, "service[memcached]"
+end
