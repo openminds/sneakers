5ded795498c60f7211d2cd184bd9ddf6749f2aab
Steven De Coeyer <steven@banteng.be>
2013-09-18 17:09:15 +0200

    Adds specs for nginx

---
+++ chef/cookbooks/nginx/spec/default_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe 'nginx::default' do
+   let(:chef_run) {
+    chef_run = ChefSpec::ChefRunner.new(platform:'debian', version:'6.0.5') do |node|
+      node.set[:base][:app_settings] = {"app_directory"=>"/Users/zhann/Desktop/app1", "type"=>"php53", "http_port"=>8010, "memory"=>1024}
+    end
+    chef_run.converge 'nginx::default'
+  }
+
+  it 'sets service nginx with action nothing' do
+    pending 'service nginx'
+  end
+
+  it 'sets nginx apt repository' do
+    pending 'apt-repo nginx'
+  end
+
+  it 'installs nginx' do
+    chef_run.should install_package 'nginx'
+  end
+
+  it 'includes nginx::configuration' do
+    chef_run.should include_recipe "nginx::configuration"
+  end
+
+  it 'includes nginx::proxy' do
+    chef_run.should include_recipe "nginx::proxy"
+  end
+end
---
+++ chef/cookbooks/nginx/spec/proxy_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe 'nginx::proxy' do
+   let(:chef_run) {
+    chef_run = ChefSpec::ChefRunner.new(platform:'debian', version:'6.0.5') do |node|
+      node.set[:base][:app_settings] = {"app_directory"=>"/Users/zhann/Desktop/app1", "type"=>"php53", "http_port"=>8010, "memory"=>1024}
+    end
+    chef_run.converge 'nginx::proxy'
+  }
+
+  it 'creates /etc/nginx/proxy_vhost.conf' do
+    file = chef_run.cookbook_file '/etc/nginx/proxy_vhost.conf'
+    file.mode.should eq '0644'
+    file.should be_owned_by 'root', 'root'
+  end
+
+  it 'creates /etc/nginx/extra.d/proxy.conf' do
+    file = chef_run.template '/etc/nginx/extra.d/proxy.conf'
+    file.mode.should eq '0644'
+    file.should be_owned_by 'root', 'root'
+  end
+
+  it 'creates /etc/nginx/conf.d/default.conf' do
+    file = chef_run.template '/etc/nginx/conf.d/default.conf'
+    file.mode.should eq '0644'
+    file.should be_owned_by 'root', 'root'
+    file.should notify 'service[nginx]', :restart
+  end
+end
