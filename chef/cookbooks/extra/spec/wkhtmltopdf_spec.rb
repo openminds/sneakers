a7737fd99fd64e5fe7991f9516cb1381705980bd
Jeroen De Vos & Steven De Coeyer <steven-en-jeroen@openminds.be>
2013-09-20 13:43:10 +0200

    Refactors extra cookbook

--- chef/cookbooks/extra/spec/wkhtmltopdf_spec.rb
+++ chef/cookbooks/extra/spec/wkhtmltopdf_spec.rb
@@ -1,14 +1,19 @@
 require 'spec_helper'
 
 describe 'extra::wkhtmltopdf' do
-   let(:chef_run) {
+  let(:chef_run) {
     chef_run = ChefSpec::ChefRunner.new(platform:'debian', version:'6.0.5') do |node|
-      node.set[:base][:app_settings] = {"app_directory"=>"/Users/zhann/Desktop/app1", "type"=>"php53", "http_port"=>8010, "memory"=>1024}
+      node.set[:base][:app_settings] = {
+        app_directory: '/Users/zhann/Desktop/app1',
+        type: 'php53',
+        http_port: 8010,
+        memory: 1024
+      }
     end
     chef_run.converge 'extra::wkhtmltopdf'
   }
 
-  %w(libxrender1 libxext6 libfontconfig1).each do |pkg|
+  %w[ libxrender1 libxext6 libfontconfig1 ].each do |pkg|
     it "installs #{pkg}" do
       chef_run.should install_package pkg
     end
@@ -23,10 +28,10 @@ describe 'extra::wkhtmltopdf' do
   end
 
   it 'extracts wkhtmltopdf' do
-    chef_run.should execute_command "tar jxvf #{File.join(Chef::Config[:file_cache_path], 'wkhtmltopdf.tar.bz2')} -C #{Chef::Config[:file_cache_path]}"
+    chef_run.should execute_command "tar jxvf #{::File.join(Chef::Config[:file_cache_path], 'wkhtmltopdf.tar.bz2')} -C #{Chef::Config[:file_cache_path]}"
   end
 
   it 'copies wkhtmltopdf to /usr/local/bin' do
-    chef_run.should execute_command "cp #{File.join(Chef::Config[:file_cache_path], chef_run.node[:wkhtmltopdf][:binary_name])} /usr/local/bin/wkhtmltopdf"
+    chef_run.should execute_command "cp #{::File.join(Chef::Config[:file_cache_path], chef_run.node[:wkhtmltopdf][:binary_name])} /usr/local/bin/wkhtmltopdf"
   end
 end
