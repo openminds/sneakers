3caa2df2de567a8b1174f4e6f9f4ad87cd95de73
Jeroen De Vos & Steven De Coeyer <steven-en-jeroen@openminds.be>
2013-09-20 16:18:34 +0200

    [base] adds missing specs for apt-keys

--- chef/cookbooks/base/spec/repositories_spec.rb
+++ chef/cookbooks/base/spec/repositories_spec.rb
@@ -41,6 +41,46 @@ describe 'base::default' do
     file.should notify 'execute[apt-key nginx]', :run, :immediately
   end
 
+  context 'php54 installation' do
+    let(:chef_run) do
+      runner = ChefSpec::ChefRunner.new(platform:'debian', version:'6.0.5')
+      runner.node.set[:php][:version] = 'php54'
+      runner.converge('base::default')
+    end
+
+    it 'creates /etc/apt/sources.list.d/dotdeb.list' do
+      file = chef_run.file '/etc/apt/sources.list.d/dotdeb.list'
+      file.mode.should eq '0644'
+      file.should be_owned_by 'root', 'root'
+      file.content.should eq "deb http://packages.dotdeb.org squeeze-php54 all"
+      file.should notify 'execute[apt-key dotdeb]', :run, :immediately
+    end
+  end
+
+  context 'php53 installation' do
+    let(:chef_run) do
+      runner = ChefSpec::ChefRunner.new(platform:'debian', version:'6.0.5')
+      runner.node.set[:php][:version] = 'php53'
+      runner.converge('base::default')
+    end
+
+    it 'creates /etc/apt/sources.list.d/dotdeb.list' do
+      file = chef_run.file '/etc/apt/sources.list.d/dotdeb.list'
+      file.mode.should eq '0644'
+      file.should be_owned_by 'root', 'root'
+      file.content.should eq "deb http://packages.dotdeb.org squeeze all"
+      file.should notify 'execute[apt-key dotdeb]', :run, :immediately
+    end
+  end
+
+  it 'creates /etc/apt/sources.list.d/mariadb.list' do
+    file = chef_run.file '/etc/apt/sources.list.d/mariadb.list'
+    file.mode.should eq '0644'
+    file.should be_owned_by 'root', 'root'
+    file.content.should eq 'deb http://mirror2.hs-esslingen.de/mariadb/repo/5.5/debian squeeze main'
+    file.should notify 'execute[apt-key mariadb]', :run, :immediately
+  end
+
   it '/etc/apt/preferences.d/dotdeb_php_pinning' do
     file = chef_run.template '/etc/apt/preferences.d/dotdeb_php_pinning'
     file.should be_owned_by 'root', 'root'
