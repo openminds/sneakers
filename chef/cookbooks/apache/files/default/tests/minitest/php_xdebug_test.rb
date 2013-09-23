d53ba93dea189a86db8961570d3b0924ecf9da4f
Jeroen De Vos <jeroenldv@gmail.com>
2013-09-23 11:43:03 +0200

    [apache::php_xdebug] Fixes php mods-available dir
    Fixes bug where Chef run fails because /etc/php5/mods-available does not exist.

--- chef/cookbooks/apache/files/default/tests/minitest/php_xdebug_test.rb
+++ chef/cookbooks/apache/files/default/tests/minitest/php_xdebug_test.rb
@@ -5,7 +5,7 @@ class TestPhp < MiniTest::Chef::TestCase
   end
 
   def test_that_the_xdebug_config_file_is_added
-    assert File.exists?("/etc/php5/fpm/conf.d/20-xdebug.ini")
+    assert File.exists?("/etc/php5/mods-available/xdebug.ini")
   end
 
   def test_that_xdebug_is_installed
@@ -13,5 +13,4 @@ class TestPhp < MiniTest::Chef::TestCase
       assert system("dpkg -l | grep #{pkg}")
     end
   end
-
 end
--- chef/cookbooks/apache/recipes/php_xdebug.rb
+++ chef/cookbooks/apache/recipes/php_xdebug.rb
@@ -4,6 +4,14 @@ node.override[:php][:html_errors] = 'On'
 
 package 'php5-xdebug'
 
+directory '/etc/php5/mods-available' do
+  owner 'root'
+  group 'root'
+  mode  '0755'
+  recursive true
+  action :create
+end
+
 template '/etc/php5/mods-available/xdebug.ini' do
   mode '0644'
   variables params: node[:xdebug]
