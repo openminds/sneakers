f58507c72ea9a43908fd07b66b6c1bab5a8ad09a
Steven De Coeyer <steven@banteng.be>
2014-02-03 11:37:40 +0100

    Escapes mysql db name

--- chef/cookbooks/mysql/recipes/databases.rb
+++ chef/cookbooks/mysql/recipes/databases.rb
@@ -1,15 +1,15 @@
 execute "create db" do
-  command "mysql -e 'create database #{node[:base][:name]}'"
+  command "mysql -e 'create database `#{node[:base][:name]}`'"
   not_if "mysql -e \"show databases\" | grep #{node[:base][:name]}"
 end
 
 execute "create db user" do
-  command "mysql -e \"grant all on #{node[:base][:name]}.* to '#{node[:base][:name]}'@'%' identified by 'vagrant'\""
+  command "mysql -e \"grant all on `#{node[:base][:name]}`.* to '`#{node[:base][:name]}`'@'%' identified by 'vagrant'\""
   not_if "mysql -e \"select * from mysql.user\" | grep % | grep vagrant"
 end
 
 execute "set db grants" do
-  command "mysql -e \"grant create view on #{node[:base][:name]}.* to '#{node[:base][:name]}'@'%' identified by 'vagrant'; grant show view on #{node[:base][:name]}.* to '#{node[:base][:name]}'@'%' identified by 'vagrant'\""
+  command "mysql -e \"grant create view on `#{node[:base][:name]}`.* to '`#{node[:base][:name]}`'@'%' identified by 'vagrant'; grant show view on `#{node[:base][:name]}`.* to '`#{node[:base][:name]}`'@'%' identified by 'vagrant'\""
   not_if "mysql -e \"select * from mysql.user\" | grep % | grep vagrant"
 end
 
